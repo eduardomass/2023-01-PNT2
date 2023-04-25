fd98b803c96943fb24efb5e2de4d41dc0a7322da
eduardomass <37450158+eduardomass@users.noreply.github.com>
2023-04-25 20:20:19 -0300

    Ejercicio

--- Clase6/ruteo/src/components/views/Index.vue
+++ Clase6/ruteo/src/components/views/Index.vue
@@ -2,7 +2,16 @@
 </script>
 
 <template>
-    Este es el home de mi pagina!!
+    <h1>Ejercicio</h1>
+    <ul>
+        <li>Realizar un menu que tenga Indice / Personajes</li>
+        <li>Crear un indice donde me de la bienvenida y sea lindo con bootstrap o lo que deseen</li>
+        <li>Crear una vista de Personajes que muestre en una tabla, cada personaje de la APi de rick and mortye (al menos los primeros 10)</li>
+        <li>Poder navegar a ver uno de los peronajes y que muestre todas sus propiedades</li>
+        <li>Hacer otro link que sea location donde muetrse todas las locaciones en una tabla</li>
+        <li>Poder ver los datos con un link a cada renglon</li>
+        <li>Desde el Personaje, poder navegar al detalle de la location!</li>
+    </ul>
 </template>
 
 <style scoped>
